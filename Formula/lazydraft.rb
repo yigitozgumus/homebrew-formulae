b345c0d6736c4746f0426131c9285ce9fc2e52a4
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2024-05-19 12:21:33 +0300

    Update lazydraft.rb

--- Formula/lazydraft.rb
+++ Formula/lazydraft.rb
@@ -4,11 +4,11 @@
 class Lazydraft < Formula
   desc "lazydraft helps you manage your markdown blog projects in your obsidian workflow. It can transfer the blog project into your static site of choice."
   homepage "https://github.com/yigitozgumus/lazydraft"
-  version "3.0.4"
+  version "3.0.6"
   license "MIT"
   url "https://github.com/yigitozgumus/lazydraft/releases/download/v3.0.5/lazydraft-mac.tar.gz"
   sha256 "7673059161985d4a7b149594c49f49b15d6ec7f77401f12661036b0720e44991"
-  version "3.0.5"
+  version "3.0.6"
 
   def install
     bin.install "lazydraft"
