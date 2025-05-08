0405e0b00d3f1c05b4e188705a35d36e6b25d76f
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2025-05-08 23:56:22 +0300

    Update interop.rb

--- Formula/interop.rb
+++ Formula/interop.rb
@@ -6,7 +6,7 @@ class Interop < Formula
   homepage "https://github.com/yigitozgumus/interop"
   version "v0.0.1"
   license "MIT"
-  url "https://github.com/yigitozgumus/interop/releases/download/v0.0.1/interop_darwin_amd64.tar.gz"
+  url "https://github.com/yigitozgumus/interop/releases/download/v0.0.1/interop_Darwin_arm64.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   version "v0.0.1"
 
